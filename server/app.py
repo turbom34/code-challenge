e1fee0b7b208732d1024bd149ef33d6bb9a6fe0e
Tim <weberk76@gmail.com>
2023-03-30 12:07:25 -0400

    finished Code challenge

--- server/app.py
+++ server/app.py
@@ -18,6 +18,102 @@ db.init_app(app)
 def home():
     return ''
 
+@app.route('/vendors', methods=['GET'])
+def vendors():
+    vendors = Vendor.query.all()
+    vendors_dict = [vendor.to_dict() for vendor in vendors]
+
+    response = make_response(
+        jsonify(vendors_dict),
+        200
+    )
+    return response
+
+@app.route('/vendors/<int:vendor_id>', methods=['GET', 'Delete'])
+def vendorbyID(id):
+    vendor = Vendor.query.filter_by(id = id).first()
+    if vendor: 
+      if request.method == 'GET':
+        vendor_dict = vendor.to_dict()
+        response = make_response(
+          jsonify(vendor_dict),
+          200
+        )
+        return response
+      
+      elif request.method == 'DELETE':
+        db.session.delete(vendor)
+        db.session.commit()
+
+        response = make_response(
+          {"error": "Vendor not found"},
+          200
+        )
+
+
+    else:
+        response = make_response(
+            {"error": "Vendor not found"},
+            404
+        )
+    return response
+
+@app.route('/vendor_sweets', methods=['POST'])
+def vendor_sweets():
+    try:
+        new_vendor_sweet = VendorSweet(
+            price = request.get_json()['price'],
+            vendor_id = request.get_json()['vendor_id'],
+            sweet_id = request.get_json()['sweet_id'],
+        )
+        db.session.add(new_vendor_sweet)
+        db.session.commit()
+
+        associated_vendor = Vendor.query.filter_by(id = new_vendor_sweet.vendor_id).first()
+        associated_vendor_dict = associated_vendor.to_dict()
+
+        response = make_response(
+            jsonify(associated_vendor_dict),
+            201
+        )
+        return response
+    except ValueError:
+        response = make_response(
+            {"error": "Price cannot be negative"},
+            400
+        )
+        return response
+    
+@app.route('/sweets/<int:id>', methods=['GET'])
+def sweetbyID(id):
+        sweet = Sweet.query.filter_by(id = id).first()
+        if sweet:
+            sweet_dict = sweet.to_dict()
+            response = make_response(
+                jsonify(sweet_dict),
+                200
+            )
+            return response
+        else:
+            response = make_response(
+                {"error": "Sweet not found"},
+                404
+            )
+            return response
+        
+@app.route('/sweets', methods=['GET'])
+def sweets():
+    sweets = sweets.query.all()
+    sweets_dict = [sweets.to_dict() for sweet in sweets]
+
+    response = make_response(
+        jsonify(sweets_dict),
+        200
+    )
+    return response
+       
+
+
     
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -12,4 +12,48 @@ db = SQLAlchemy(metadata=metadata)
 
 # Add models here
 
+class Sweet(db.Model, SerializerMixin):
+    __tablename__ = 'sweets'
+
+    serialize_rules = ('-vendors.sweets', '-vendor_sweets.sweets')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    vendor_sweets = db.relationship('VendorSweet', backref='sweet')
+
+class Vendor(db.Model, SerializerMixin):
+    __tablename__ = 'vendors'
+
+    serialize_rules = ('-vendor_sweets.sweet', '-vendor_sweets.vendor')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    vendor_sweets = db.relationship('VendorSweet', backref='vendor')
+
+class VendorSweet(db.Model, SerializerMixin):
+    __tablename__ = 'vendor_sweets'
+
+    serialize_rules = ('-vendor.vendor_sweets', '-sweet.vendor_sweets')
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    sweets_id = db.Column(db.Integer, db.ForeignKey('sweets.id'))
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendors.id'))
+
+
+    @validates('price')
+    def validate_price(self, key, price):
+        if price < 0:
+            raise ValueError('Price cannot be negative')
+        return price
+
 # for relationships between tables, name these "vendor_sweets"
